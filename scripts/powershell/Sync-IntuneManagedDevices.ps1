0d7dbe79131afa3d609ff583bd9c454165e05252
builtbyke <builtbyke.com>
2026-04-22 21:25:49 -0400

    added Sync-IntuneManagedDevices.ps1

---
+++ scripts/powershell/Sync-IntuneManagedDevices.ps1
@@ -0,0 +1,34 @@
+<#
+.SYNOPSIS
+    Exports managed Intune devices to a CSV and performs a sync on each device
+
+.NOTES
+The below modules need to be installed in order to run this script:
+
+1. Install-Module -Name Microsoft.Graph
+2. Install-Module -Name Import-Excel
+
+Make sure you are connected to Microsoft Graph, if not, use this command and authenticate:
+
+** Connect-MgGraph -Scopes "DeviceManagementManagedDevices.PrivilegedOperations.All" **
+#>
+
+Get-MgDeviceManagementManagedDevice | Export-Csv -Path 'path'
+
+$pathToCsv = 'path'
+$file = Import-Csv -Path $pathToCsv | Select 'Id'
+
+foreach ($row in $file) {
+
+    Write-Host "Attempting sync on device with ID: $($row.Id)" -ForegroundColor Green
+    Sync-MgDeviceManagementManagedDevice -ManagedDeviceId $row.Id
+
+try {
+    Sync-MgDeviceManagementManagedDevice -ManagedDeviceId $row.Id -ErrorAction Stop
+    Write-Host "This device was successfully synced" -ForegroundColor Green
+
+} 
+catch {
+    Write-Host "Could not sync device $($row.Id), please try again" -ForegroundColor Yellow
+}
+}
